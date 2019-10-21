3462f4c357e7f77f0311eab7588e64c884e6784d
Kezia Baidoo <keziabaidoo100@gmail.com>
2019-10-21 16:50:30 +0000

    js

---
+++ myscript.js
@@ -0,0 +1,12 @@
+var numH=268
+var numK=199
+var numM=200
+
+
+let firstname="kesia"
+let lastname="baidoo"
+let surname="adjoa"
+
+
+console.log("<h1>" + numM+numH +"</h1>")
+console.log("<h3>" + firstname+surname +"</h4>")
